27f1dc47f7182d8c069967542179483d73617a42
bakito <github@bakito.ch>
2019-02-19 22:35:43 +0100

    remove components

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,18 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
 import { render } from "react-dom";
-import FormContainer from "./containers/FormContainer";
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="col-md-6">
-        <h3> Sample Form Container </h3>
-        <FormContainer />
-      </div>
-    );
-  }
-}
+import App from "./App";
 
 render(<App />, document.getElementById("root"));
